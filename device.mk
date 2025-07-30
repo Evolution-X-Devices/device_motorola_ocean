62c8dd51eeace687dc5815e8a239a3f24a4ee356
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-30 01:14:06 -0400

    ocean: Move nfc configs to vendor
    Change-Id: Ie418ce8f7ecdec582e6e139e230e6a9e067f745f

--- device.mk
+++ device.mk
@@ -52,10 +52,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.uicc.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     Tag \
